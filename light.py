4650a7ab3d28834d608728a3b719e26ce12542a0
mmikhov <mmikhov@lohika.com>
2018-09-05 17:13:25 +0300

    Light module fixes

--- light.py
+++ light.py
@@ -11,10 +11,12 @@ def switchingOnTheLight():
 	else:
 		#Switching on the light
 		return "Light switching on"
+		lightStatus = True
 
 def switchingOffTheLight():
 	if lightStatus:
 		#Switching off the light
 		return "Light switching off"
+		lightStatus = False
 	else:
 		return "Light is already switched off"
