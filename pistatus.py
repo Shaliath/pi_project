347bf473c5bacb2074c4ef8987ad369c85276fe0
mmikhov <mmikhov@lohika.com>
2018-09-05 12:41:45 +0300

    Added shell command

---
+++ pistatus.py
@@ -0,0 +1,6 @@
+import subprocess
+
+def cpuTemp():
+	first = int(subprocess.Popen("cat /sys/devices/virtual/thermal/thermal_zone1/temp", shell = True, stdout = subprocess.PIPE).stdout.read())
+	second = int(subprocess.Popen("cat /sys/devices/virtual/thermal/thermal_zone0/temp", shell = True, stdout = subprocess.PIPE).stdout.read())
+	return str((first+second)/2.0)
